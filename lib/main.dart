68d96c9830c45ba5a6378be484419072a75843c6
Redul94 <redul15-3193@diu.edu.bd>
2024-08-29 22:23:53 +0600

    update variables

--- lib/main.dart
+++ lib/main.dart
@@ -2,25 +2,26 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MaterialApp(
-    home: NinjaCard(),
+    home: Card(),
   ));
 }
 
-class NinjaCard extends StatefulWidget {
-  const NinjaCard({super.key});
+class Card extends StatefulWidget {
+  const Card({super.key});
 
   @override
-  State<NinjaCard> createState() => _NinjaCardState();
+  State<Card> createState() => _CardState();
 }
 
-class _NinjaCardState extends State<NinjaCard> {
+class _CardState extends State<Card> {
   int ninjaLevel = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[900],
       appBar: AppBar(
-        title: const Text('Ninja Id card'),
+        title: const Text('ID Card'),
         centerTitle: true,
         backgroundColor: Colors.grey[850],
         elevation: 0.0,
@@ -31,20 +32,19 @@ class _NinjaCardState extends State<NinjaCard> {
             ninjaLevel += 1;
           });
         },
-        child: Icon(Icons.add),
-        backgroundColor: Colors.grey,
+        backgroundColor: Colors.grey, 
+        child: const Icon(Icons.add), 
       ),
       body: Padding(
         padding: const EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
-          // ignore: prefer_const_literals_to_create_immutables
           children: <Widget>[
             const CircleAvatar(
-              backgroundImage: AssetImage('asse
-              ts/image4.jpg'),
+              backgroundImage: AssetImage('assets/image4.jpg'),
               radius: 50.0,
             ),
+            const SizedBox(height: 15.0),
             const Text(
               'Name',
               style: TextStyle(
@@ -54,7 +54,7 @@ class _NinjaCardState extends State<NinjaCard> {
             ),
             const SizedBox(height: 15.0),
             const Text(
-              'Chung-Li',
+              'Redul Hossen',
               style: TextStyle(
                 color: Colors.amberAccent,
                 letterSpacing: 2.0,
@@ -64,33 +64,32 @@ class _NinjaCardState extends State<NinjaCard> {
             ),
             const SizedBox(height: 15.0),
             const Text(
-              'Current Ninja Level',
+              'Current Level',
               style: TextStyle(
                 color: Colors.grey,
                 letterSpacing: 2.0,
               ),
             ),
             const SizedBox(height: 10.0),
-            Text('$ninjaLevel',
-                style: TextStyle(
-                  color: Colors.amberAccent,
-                  letterSpacing: 2.0,
-                  fontSize: 26.0,
-                  fontWeight: FontWeight.bold,
-                )),
+            Text(
+              '$ninjaLevel',
+              style: const TextStyle(
+                color: Colors.amberAccent,
+                letterSpacing: 2.0,
+                fontSize: 26.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
             const SizedBox(height: 30.0),
             Row(
-              // ignore: prefer_const_literals_to_create_immutables
               children: [
-                // ignore: prefer_const_constructors
                 Icon(
                   Icons.email,
                   color: Colors.grey[400],
                 ),
                 const SizedBox(width: 10.0),
-
                 Text(
-                  'chun-li@gmail.com',
+                  'redulhossen94@gmail.com',
                   style: TextStyle(
                     color: Colors.grey[400],
                     fontSize: 20.0,
